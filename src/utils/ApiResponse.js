e60a3c82ed7234ae99be6039ae4324fd02794a45
kishor-abhijeet <allinoneallinone1245@gmail.com>
2025-08-20 20:31:18 +0530

    models defining password enscription

--- src/utils/ApiResponse.js
+++ src/utils/ApiResponse.js
@@ -3,6 +3,6 @@ class ApiResponse{
         this.statusCode = statusCode
         this.data = data
         this.message = message
-        this.scuccess = statusCode < 400
+        this.success = statusCode < 400 
     }
 }
